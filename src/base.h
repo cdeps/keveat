51a345f8ebda56652ec2d3ee610b9410b7dd4d1d
Robin Bron <robin@finwo.nl>
2017-12-05 21:38:48 +0100

    Initial commit

---
+++ src/base.h
@@ -0,0 +1,14 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef H_BASE
+#define H_BASE
+
+
+
+#endif // H_BASE
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
---
+++ src/main.c
@@ -0,0 +1,13 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "base.h"
+
+int main( int argc, char *argv[] ) {
+  return 42;
+}
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
