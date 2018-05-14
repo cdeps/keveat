cce37ca4e2ae7d57ccfb950fea1e2a5805a1a896
Robin Bron <robin@finwo.nl>
2018-05-14 21:04:17 +0200

    The only function that doesn't need explaining

--- src/keveat.c
+++ src/keveat.c
@@ -4,6 +4,14 @@ extern "C" {
 
 #include "keveat.h"
 
+#include <stdlib.h>
+
+keveat_ctx * keveat_open( int fd ) {
+  keveat_ctx *output = calloc( 1, sizeof(keveat_ctx) );
+  output->fd = fd;
+  return output;
+}
+
 #ifdef __cplusplus
 } // extern "C"
 #endif
--- src/keveat.h
+++ src/keveat.h
@@ -5,6 +5,23 @@ extern "C" {
 #ifndef ___KEVEAT_H_
 #define ___KEVEAT_H_
 
+#include <stdio.h>
+
+typedef struct {
+  int fd;
+} keveat_ctx;
+
+/**
+ * Open a file descriptor as a keveat collection
+ *
+ * While this creates a new context, it DOES NOT verify if the given file
+ * descriptor contains a valid collection.
+ *
+ * @param {int} fd       File descriptor to operate on
+ *
+ * @return {keveat_ctx}  The context to pass to later calls
+ */
+keveat_ctx * keveat_open( int fd );
 
 
 #endif // ___KEVEAT_H_
