03d060dd8fc3dfe6f6f3ea310d056edc858d00be
Robin Bron <robin@finwo.nl>
2018-05-15 22:17:04 +0200

    Making sure everything is simple and structured this time

--- src/keveat.c
+++ src/keveat.c
@@ -4,13 +4,73 @@ extern "C" {
 
 #include "keveat.h"
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 keveat_ctx * keveat_open( int fd ) {
+
+  // Prepare output & check memory
   keveat_ctx *output = calloc( 1, sizeof(keveat_ctx) );
+  char *check = malloc( 9 );
+
+  // Fetch first 9 bytes for header check
+  fseek( fd, 0, SEEK_SET );
+  read( fd, check, 9 );
+
+  // Allow newline after start with "KEVEAT"
+  if ( *(check+6) == '\n' ) {
+    *(check+6) = '\0';
+  }
+
+  // We may skip the first 2 bytes for hard drives (MBR)
+  // Make sure the buffer ends in \0 to prevent segfault
+  *(check+8) = '\0';
+
+  // Run the actual check
+  // Fail = stop right now
+  if ( strcmp("KEVEAT", check   ) != 0 &&
+       strcmp("KEVEAT", check+2 ) != 0 ) {
+    free(check);
+    free(output);
+    return NULL;
+  }
+
+  // We're here so we're good
+
+  // TODO
+  // Read metadata
+  //   - Header-size   : optional, may be indicated through double line feed '\n'
+  //   - Pending       : not given = read-only collection
+  //   - Copy-on-write : not given = read-only collection
+
   output->fd = fd;
   return output;
 }
+
+
+
+// /**
+// * Gracefully shutdown the collection after finishing all requests
+// *
+// * Enqueues a shutdown to execute & prevents new tasks to be given
+// *
+// * @param {keveat_ctx*}               ctx       The collection context to work upon
+// * @param {function(keveat_event*)*}  callback  The function that will be called once this has completed
+// * @param {*}                         udata     Data to pass to the callback
+// */
+// void keveat_shutdown( keveat_ctx *ctx, void (*callback)(struct keveat_event *), void *udata );
+
+// /**
+// * Shutdown the collection NOW
+// *
+// * Instantly stops the collection without running any tasks. You will lose all
+// * data that is in the queue and has not been written yet.
+// *
+// * @param {keveat_ctx*}  ctx  The collection to shut down
+// */
+// void keveat_destroy( keveat_ctx *ctx );
 
 #ifdef __cplusplus
 } // extern "C"
--- src/keveat.h
+++ src/keveat.h
@@ -7,21 +7,69 @@ extern "C" {
 
 #include <stdio.h>
 
-typedef struct {
-  int fd;
+#define ___KEVEAT_FLAG_READ     1
+#define ___KEVEAT_FLAG_WRITE    2
+#define ___KEVEAT_FLAG_DELETE   3
+#define ___KEVEAT_FLAG_COMPACT  4
+#define ___KEVEAT_FLAG_SHUTDOWN 5
+
+struct {
+  char *name;   // Name of the event
+  void *udata;  // User data
+} keveat_event;
+
+struct {
+  void          *next;                     // Pointer to the next listener
+  unsigned char flags;                     // Internal flags (read,write,delete,compact,shutdown)
+  char          *name;                     // Name or key do update
+  void          *data;                     // Data to write
+  void          *udata;                    // Data to pass to the callback
+  void (*callback)(struct keveat_event *); // Callback to run once the action/event happened
+} keveat_listener;
+
+struct {
+  int fd;                             // The file descriptor this collection acts on
+  unsigned long long     start;       // Offset to location where the actual data starts a.k.a.
+  unsigned long long     pending;     // +start = where the pending data starts (non-compacted)
+  unsigned long long     cow;         // +start = where to write new pending data
+  struct keveat_listener *listeners;  // Listeners for internal events
+  struct keveat_listener *tasks;      // Tasks to execute
 } keveat_ctx;
 
 /**
  * Open a file descriptor as a keveat collection
  *
- * While this creates a new context, it DOES NOT verify if the given file
- * descriptor contains a valid collection.
+ * Creates a new context & verifies the given file descriptor contains valid
+ * collection data.
  *
- * @param {int} fd        File descriptor to operate on
+ * @param {int}                       fd        File descriptor to operate on
+ * @param {function(keveat_event*)*}  callback  The function that will be called once this collection has started.
+ * @param {*}                         udata     Data to pass to the callback
  *
- * @return {keveat_ctx*}  The context to pass to later calls
+ * @return {keveat_ctx*}             The context to pass to later calls
  */
-keveat_ctx * keveat_open( int fd );
+struct keveat_ctx * keveat_open( int fd, void (*callback)(struct keveat_event *), void *udata );
+
+/**
+ * Gracefully shutdown the collection after finishing all requests
+ *
+ * Enqueues a shutdown to execute & prevents new tasks to be given
+ *
+ * @param {keveat_ctx*}               ctx       The collection context to work upon
+ * @param {function(keveat_event*)*}  callback  The function that will be called once this has completed
+ * @param {*}                         udata     Data to pass to the callback
+ */
+void keveat_shutdown( keveat_ctx *ctx, void (*callback)(struct keveat_event *), void *udata );
+
+/**
+ * Shutdown the collection NOW
+ *
+ * Instantly stops the collection without running any tasks. You will lose all
+ * data that is in the queue and has not been written yet.
+ *
+ * @param {keveat_ctx*}  ctx  The collection to shut down
+ */
+void keveat_destroy( keveat_ctx *ctx );
 
 #endif // ___KEVEAT_H_
 
